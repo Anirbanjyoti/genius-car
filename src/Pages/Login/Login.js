223767311f376e34934a6ae0fa2621eed805ec45
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-17 18:04:38 +0530

    login functionality by useRef additional hook

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,41 +1,56 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import './Login.css'
+import React, { useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const handleRegister=()=>{
-        navigate('/register')
-    }
-    return (
-        <div>
-        <div className="container log-container">
-        <div className="row">
-            <h1>This is Login Page</h1>
-            <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+// useRef
+    const emailRef = useRef(' '); 
+    const passwordRef= useRef(' '); 
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    <p>New to Genius Car? <button onClick={handleRegister}>Please Register!</button></p>
+    const handleUserLogin =event=>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+        
+    }
+
+    const navigate = useNavigate();
+  const handleRegister = () => {
+    navigate("/register");
+  };
+  return (
+    <div>
+      <div className="container log-container">
+        <div className="row">
+          <h1>This is Login Page</h1>
+          <Form onSubmit={handleUserLogin}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <p>
+            New to Genius Car?{" "}
+            <button onClick={handleRegister}>Please Register!</button>
+          </p>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,4 +1,4 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import React from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -6,43 +6,49 @@ import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <div>
-        <div className="container log-container">
+      <div className="container log-container">
         <div className="row">
           <h1>This is Registration Page</h1>
-          
+
           <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="Text" placeholder="Enter Your Name" />
+              <Form.Text className="text-muted">Enter Your Name.</Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
 
           <p>
             If you have account?
-            <Link to='/login'>Please Login!</Link>
+            <Link to="/login">Please Login!</Link>
           </p>
         </div>
-        </div>
-        </div>
+      </div>
+    </div>
   );
 };
 
