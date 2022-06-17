ab8a6f8d43a2c2beb87e014110453c5dc372b95b
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-17 18:59:25 +0530

    firebase.init.js secured by use environment variable in firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_14mHRaSargCQnHUzabnzRLjCYynCa0Q",
-  authDomain: "genius-car-b492a.firebaseapp.com",
-  projectId: "genius-car-b492a",
-  storageBucket: "genius-car-b492a.appspot.com",
-  messagingSenderId: "63992472",
-  appId: "1:63992472:web:f7122b5e2233cfad8096ed"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
